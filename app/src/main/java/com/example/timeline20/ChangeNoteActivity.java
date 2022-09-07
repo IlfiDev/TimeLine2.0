eb6d0104961d3ff4627920498914f74b9269cb5f
IlfiDev <ilyafillipov2@gamil.com>
2022-09-07 17:40:10 +0300

    Added Note Class

--- app/src/main/java/com/example/timeline20/ChangeNoteActivity.java
+++ app/src/main/java/com/example/timeline20/ChangeNoteActivity.java
@@ -10,5 +10,6 @@ public class ChangeNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
     }
 }
