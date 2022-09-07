3a2248add32816ed52e17904fca314045881d8ab
IlfiDev <ilyafillipov2@gamil.com>
2022-09-07 12:06:03 +0300

    Added Button and activity

--- app/src/main/java/com/example/timeline20/MainActivity.java
+++ app/src/main/java/com/example/timeline20/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.timeline20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OpenNoteActivity(View view) {
+        Intent intent = new Intent(this, CreateNoteActivity.class);
+        startActivity(intent);
     }
 }
